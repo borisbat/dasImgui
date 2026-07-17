ce723c060af313d7af700423ac92245ff20649e3
Boris Batkin <bbatkin@gmail.com>
2026-07-17 12:51:33 -0700

    dasImgui: fix portable build and lint gate

--- src/dasIMGUI.markdown.cpp
+++ src/dasIMGUI.markdown.cpp
@@ -1,6 +1,7 @@
 #include "daScript/misc/platform.h"
 #include "daScript/ast/ast.h"
 #include "daScript/ast/ast_interop.h"
+#include "daScript/ast/ast_handle.h"
 #include "dasIMGUI.h"
 #include "aot_dasIMGUI.h"
 
